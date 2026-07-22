3dfa988c2cf1612ca4f3233c4217f73ca89638df
coolnether123 <32144172+coolnether123@users.noreply.github.com>
2026-07-22 15:35:29 -0500

    Fix typing task word selection

--- scenes/tasks/typing_task.gd
+++ scenes/tasks/typing_task.gd
@@ -8,8 +8,10 @@ func _task_ready() -> void:
 
 func _pick_word() -> String:
 	var file_contents : String = FileAccess.open(path_to_task_words, FileAccess.READ).get_as_text()
-	var words : Array[String] = file_contents.split("\n")
-	var chosen_word : String = ""
-	while not chosen_word.contains("#") and chosen_word.length() > 0:
-		chosen_word = words.pick_random()
-	return chosen_word
+	var words := file_contents.split("\n")
+	var valid_words: Array[String] = []
+	for word in words:
+		var stripped_word := word.strip_edges()
+		if not stripped_word.is_empty() and not stripped_word.begins_with("#"):
+			valid_words.append(stripped_word)
+	return valid_words.pick_random() if not valid_words.is_empty() else ""
