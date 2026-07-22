95be0120972be84d7707e0d58056c75f0809eeff
Caspian Tyler <caspian@tylerinternet.com>
2026-07-22 16:33:31 -0400

    word selected

--- Scenes/Tasks/typing_task.gd
+++ Scenes/Tasks/typing_task.gd
@@ -1,8 +1,15 @@
 extends Task
 
+@export var path_to_task_words : String = "res://Resources/WordLibrary.txt"
 
 func _task_ready() -> void:
+	print(_pick_word())
 	pass # Replace with function body.
 
 func _pick_word() -> String:
-	return ""
+	var file_contents : String = FileAccess.open(path_to_task_words, FileAccess.READ).get_as_text()
+	var words : Array[String] = file_contents.split("\n")
+	var chosen_word : String = ""
+	while not chosen_word.contains("#") and chosen_word.length() > 0:
+		chosen_word = words.pick_random()
+	return chosen_word
