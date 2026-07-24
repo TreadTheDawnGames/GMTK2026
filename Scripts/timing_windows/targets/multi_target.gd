2566fed25f81854dea3aa6db150916eea6d556ae
Caspian Tyler <caspian@tylerinternet.com>
2026-07-24 00:44:23 -0400

    Chase target finished

--- Scripts/timing_windows/targets/multi_target.gd
+++ Scripts/timing_windows/targets/multi_target.gd
@@ -1,30 +1,44 @@
 extends TimingTarget
-class_name MultiTarget
+class_name ChaseTarget
 
 @onready var times_hit_label: Label = %TimesHitLabel
 
 @export var required_hits:int = 3
-var times_hit : int = 0
+var times_hit : int = 0:
+	set(value):
+		times_hit = value
+		times_hit_label.text = str(required_hits-times_hit)
+		
 
 var slider_position : float = 0.0
 var slider_direction : float = 0.0
+var distance : float = 150.0
 
 func initialize():
-	times_hit_label.text = str(times_hit_label)
+	super.initialize()
+	times_hit_label = %TimesHitLabel
+	times_hit_label.text = str(required_hits)
 
 func hit(_timing_window : SliderTimingWindow = null) -> void:
-	if times_hit == required_hits:
-		super.hit(_timing_window)
-		return
+
 	times_hit += 1
 	slider_position = _timing_window.slider.position.x
 	slider_direction = _timing_window.direction
-	#place(_timing_window.backing.size.x)
+	_timing_window.randomize_target(self)
+	if times_hit == required_hits:
+		super.hit(_timing_window)
+		times_hit = 0
 	pass
-	
+
+func unhit():
+	super.unhit()
+	times_hit = 0
 
 func place(placement_width : float) -> float:
-	
-	
-	
-	return 0.0
+	var placement : float = slider_position + (distance * slider_direction)
+	if placement > placement_width:
+		placement = placement_width - distance
+	if placement < 0.0:
+		placement = distance
+		
+	return placement
